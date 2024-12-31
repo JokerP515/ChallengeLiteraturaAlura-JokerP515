59aa4743cb1460f6eec1fdaa47a781c4c8af261c
David Diaz <josedavid2010d@gmail.com>
2024-12-31 13:22:51 -0500

    First commit

---
+++ challenge-literatura-jokerp515/src/main/java/com/alurachallenge/literatura/jokerp515/challenge_literatura_jokerp515/ChallengeLiteraturaJokerp515Application.java
@@ -0,0 +1,13 @@
+package com.alurachallenge.literatura.jokerp515.challenge_literatura_jokerp515;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLiteraturaJokerp515Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLiteraturaJokerp515Application.class, args);
+	}
+
+}
---
+++ challenge-literatura-jokerp515/src/test/java/com/alurachallenge/literatura/jokerp515/challenge_literatura_jokerp515/ChallengeLiteraturaJokerp515ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alurachallenge.literatura.jokerp515.challenge_literatura_jokerp515;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeLiteraturaJokerp515ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
